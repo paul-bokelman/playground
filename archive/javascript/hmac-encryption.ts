249882e560f713480d44bca3240429672f51701a
Paul Bokelman <paul.bokelman1@gmail.com>
2023-11-17 18:45:28 -0700

    completed hmac enc

--- archive/javascript/hmac-encryption.ts
+++ archive/javascript/hmac-encryption.ts
@@ -1,54 +1,65 @@
 // npm run ts -- ./archive/javascript/hmac-encryption.ts
+import fs from "node:fs";
 import crypto from "node:crypto";
 
 const algorithm = "aes-192-cbc";
 
-const iv = crypto.randomBytes(16);
-
-const to_key = (password: string) => crypto.scryptSync(password, "salt", 24);
+const hash = (password: string) => crypto.scryptSync(password, "salt", 24);
+const iv = hash("secret").subarray(0, 16);
 
 const hmac = (data: string, password: string) => {
-  const hmac = crypto.createHmac("sha256", to_key(password));
+  const hmac = crypto.createHmac("sha256", hash(password));
   hmac.update(password);
   hmac.update(data);
   return hmac.digest("hex");
 };
 
 const encrypt = (data: string, password: string): { encrypted: string; hmac: string } => {
-  const key = to_key(password);
+  const key = hash(password);
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   const encrypted = cipher.update(data, "utf8", "hex") + cipher.final("hex");
   return { encrypted, hmac: hmac(data, key.toString("hex")) };
 };
 
-const decrypt = ({ encrypted, hmac: receivedHmac }: { encrypted: string; hmac: string }, password: string) => {
-  const key = to_key(password);
+const decrypt = ([encrypted, receivedHmac]: [string, string], password: string) => {
+  const key = hash(password);
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
 
   try {
     const decrypted = decipher.update(encrypted, "hex", "utf8") + decipher.final("utf8");
-    if (hmac(decrypted, key.toString("hex")) !== receivedHmac) {
-      return console.log("integrity invalid");
-    }
-    return console.log(decrypted);
+    if (hmac(decrypted, key.toString("hex")) !== receivedHmac) return Error("Integrity compromised");
+    return decrypted;
   } catch (e) {
-    return console.log("invalid password");
+    // if data is tampered and block length is not valid then this will throw invalid password...
+    return Error("Invalid Password");
   }
 };
 
-const real_data = JSON.stringify([
-  { name: "test", desc: "random" },
-  { name: "test2", desc: "random2" },
-]);
+const file = "./out/hmac-enc/test.txt";
+const password = "password124";
 
-const invalid_data = JSON.stringify([
-  { name: "test", desc: "random" },
-  { name: "test2", desc: "random1" },
-]);
+const writeTestFile = () => {
+  const d = Object.values(encrypt(JSON.stringify([{ name: "valid", desc: "data" }]), password)).join(",");
+  fs.writeFileSync(file, d, "utf-8");
+};
 
-const enc_data = encrypt(real_data, "password123");
-const invalid_enc_data = encrypt(invalid_data, "password123");
+const writeDifferentData = () => {
+  const prevHMAC = fs.readFileSync(file, "utf-8").split(",")[1];
+  const d = encrypt(JSON.stringify([{ name: "invalid", desc: "data!" }]), password).encrypted;
+  fs.writeFileSync(file, [d, prevHMAC].join(","), "utf-8");
+};
 
-decrypt({ encrypted: enc_data.encrypted, hmac: invalid_enc_data.hmac }, "password123"); // -> integrity invalid
-decrypt(enc_data, "password124"); // -> incorrect password
-decrypt(enc_data, "password123"); // -> data
+// writeTestFile();
+// writeDifferentData();
+
+(() => {
+  const tests = [decrypt(fs.readFileSync(file, "utf-8").split(",") as [string, string], password)];
+
+  for (const [index, test] of tests.entries()) {
+    if (test instanceof Error) {
+      console.log("Error:", index, test.message);
+    } else {
+      console.log("Data:", index, test);
+    }
+  }
+})();
