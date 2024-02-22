01b1bb806d7daf8bed777149120017461c26dd04
Paul Bokelman <paul.bokelman1@gmail.com>
2024-02-22 11:21:10 -0700

    yuh

---
+++ archive/python/binary-conversion.py
@@ -0,0 +1,27 @@
+import math
+
+def decimalToBinary(decimal: int) -> str:
+    n = 0
+    while(decimal > math.pow(2, n + 1)):
+        n = n + 1
+    ordered_binary_values = [0] * int(n + 1)
+    for current_n in reversed(list(range(0, n + 1))):
+        if(decimal == 0): break
+        current_n_value = int(math.pow(2, current_n))
+        if(current_n_value <= decimal):
+            ordered_binary_values[(len(ordered_binary_values) - current_n) - 1] = current_n_value
+            decimal = decimal - current_n_value
+    return "".join([str(1) if v != 0 else str(0) for v in ordered_binary_values])
+
+def binaryToDecimal(bs: str) -> int:
+    binary_values = []
+    for index, value in enumerate(bs):
+        value = int(value)
+        if(value == 0):
+            continue
+        binary_values.append(math.pow(2, len(bs) - 1 - index))
+    return int(sum(binary_values))
+
+binary_string = decimalToBinary(8324)
+value = binaryToDecimal(binary_string)
+print(value) # 8324
---
+++ archive/python/prime-factorization.py
@@ -0,0 +1,29 @@
+# python3 ./archive/python/prime-factorization.py
+
+lowest_prime_factors = [2, 3, 5, 7]
+
+# only works for first 4 prime factors
+def prime_factorization(x):
+    factors = []
+    while x % 2 == 0:
+        x = x / 2
+        factors.append(2)
+
+    prev_x = 0
+    while(True):
+        if(prev_x is x): # factor wasn't divisible by 
+            if(int(x) != 1):
+                factors.append(int(x))
+            return factors
+        else:
+            prev_x = x
+        for pf in lowest_prime_factors[1:]:
+            if(x % pf == 0):
+                x = x / pf
+                factors.append(pf)
+                continue
+
+
+
+
+print(prime_factorization(2))
