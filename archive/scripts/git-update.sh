15f04ada06a0af110b227c7cb095207632da9d1d
Paul Bokelman <paul.bokelman1@gmail.com>
2024-10-14 21:59:57 -0600

    git update

--- archive/scripts/git-update.sh
+++ archive/scripts/git-update.sh
@@ -3,24 +3,8 @@
 isOnline=$(ping -q -c1 google.com &>/dev/null && echo online || echo offline)
 
 if [ "$isOnline" = "online" ]; then
-    echo "hi"
+    git restore .
+    git pull
 else
     echo "Wi-Fi not connected. Skipping repo update."
 fi
-
-
-# # Check if Wi-Fi is connected
-# if iwconfig 2>&1 | grep -q "ESSID:\""; then
-#     # cd /home/chandelier2/Desktop/chandelier
-
-#     # # update the code to latest
-#     # git restore .
-#     # git pull
-
-#     # sudo reboot
-
-#     echo "hi"
-    
-# else
-#     echo "Wi-Fi not connected. Skipping repo update."
-# fi
